a4a391b0698031ad1a72c8154d2c1f96105678c9
Kirill Pereshitov <44980391+krlprtv@users.noreply.github.com>
2021-02-10 11:46:33 +0300

    Timer update

--- v1.0.py
+++ v1.0.py
@@ -1,11 +1,11 @@
 from typing import List
-
+import timeit
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup, ResultSet
 
 urls = pd.read_csv(r'C:\Users\krlpr\Downloads\urls.csv')
-
+start = timeit.default_timer()
 tovar_id = []
 tovar_title = []
 tovar_price = []
@@ -15,19 +15,15 @@ headers = {
     'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
                   "Chrome/55.0.2883.87 Safari/537.36"}
 
-for i in len(urls.shape[0]):
-    response = requests.get(urls['loc'][i], headers = headers)
-    # Можно попробовать занметь html на lxml
-    soup = BeautifulSoup(response.text, 'html.parser')
-    tovar_title.append(soup.find('h1').text)
-    tovar_price.append(soup.find('div', class_="b-goods-price__sum b-price b-price_large js-price").text)
-    tovar_category.append(soup.find('span', class_="b-product-menu__title").text)
-
-# Попробовать такой подход
-# tovar_ids_ = soup.find_all('div, class_"b-goods-price__sum b-price b-price_large js-price")
-# Или
-# tovar_ids = soup.find('div', {'class':'b-goods-price__sum b-price b-price_large js-price'}).find('b') Добавить find('b') в 23 строку
-
+for i in range(15000):
+    try:
+        response = requests.get(urls['loc'][i], headers = headers)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        tovar_title.append(soup.find('h1').text)
+        tovar_price.append(soup.find('div', class_="b-goods-price__sum b-price b-price_large js-price").find('b').text)
+        tovar_category.append(soup.find('span', class_="b-product-menu__title").text)
+    except (AttributeError):
+        tovar_price.append('No value')
 
 
 df = pd.DataFrame(
@@ -37,5 +33,5 @@ df = pd.DataFrame(
      },
     columns=['Title', 'Price', 'Category']
 )
-
-df.to_excel(r'C:\Users\krlpr\Downloads\urls100.xlsx', index=False)
+print(tovar_price)
+df.to_excel(r'C:\Users\krlpr\Downloads\data15000.xlsx', index=False)
